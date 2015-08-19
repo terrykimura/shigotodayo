e1a21739162257b876e7a4745af1bab875ee938b
terrykimura <terry.kimura@gmail.com>
2015-08-19 09:21:51 +0000

    Added edit and delete

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,8 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [:edit, :update, :destroy]
   def index
     @jobs = Job.all
+    @job = Job.new
   end
   
   def create
@@ -9,8 +11,28 @@ class JobsController < ApplicationController
     redirect_to root_path , notice: 'お仕事を保存しました'
   end
   
+  def edit
+  end
+  
+  def update
+    if @job.update(job_params)
+      redirect_to root_path , notice: 'お仕事を保存しました'
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @job.destroy
+    redirect_to root_path, notice: 'お仕事を削除しました'
+  end
+  
   private
   def job_params
     params.require(:job).permit(:name, :address, :stationline, :station, :stationdistance, :description, :requiredskills, :salary, :hours, :overtime, :holidays, :welfare)
+  end
+  
+  def set_job
+    @job = Job.find(params[:id])
   end
 end
