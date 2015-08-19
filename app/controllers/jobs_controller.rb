52bf3abb80bf0ad45248a3582f9d996495ab9555
terrykimura <terry.kimura@gmail.com>
2015-08-19 08:50:40 +0000

    Created job/index, list of jobs and creator form for jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,16 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
+  end
+  
+  def create
+    @job = Job.new(job_params)
+    @job.save
+    redirect_to root_path , notice: 'お仕事を保存しました'
+  end
+  
+  private
+  def job_params
+    params.require(:job).permit(:name, :address, :stationline, :station, :stationdistance, :description, :requiredskills, :salary, :hours, :overtime, :holidays, :welfare)
   end
 end
